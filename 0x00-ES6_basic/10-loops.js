ef70fe4458165ce17465e7a0de055c0b31e0fb9e
Salsabil <salsabeelahmed323@gmail.com>
2024-07-22 21:35:09 +0300

    task 10

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const arrayend=[];
+  for (const idx of array) {
+    arrayend.push(`${appendString}${idx}`);
+  }
+
+  return arrayend;
+}
