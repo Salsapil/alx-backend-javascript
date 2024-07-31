dd25084a6071b6b8e83222fe60b38213061334f4
Salsabil <salsabeelahmed323@gmail.com>
2024-07-31 03:11:51 +0300

    task 11

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,14 @@
+export const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+
+  const currentCount = weakMap.get(endpoint);
+  if (currentCount >= 4) {
+    throw new Error('Endpoint load is high');
+  }
+
+  weakMap.set(endpoint, currentCount + 1);
+}
