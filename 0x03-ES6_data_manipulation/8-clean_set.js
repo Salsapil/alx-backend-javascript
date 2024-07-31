164a7632347b405ca08da55271c9b6a1c7fe90d6
Salsabil <salsabeelahmed323@gmail.com>
2024-07-31 03:06:57 +0300

    task 8 & 10

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,10 @@
+export default function cleanSet(set, startString) {
+  const strings = [];
+  if (startString === '' || typeof startString !== 'string') return '';
+  set.forEach((str) => {
+    if (typeof str === 'string' && str.startsWith(startString)) {
+      strings.push(str.slice(startString.length));
+    }
+  });
+  return strings.join('-');
+}
