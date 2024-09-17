ae3b4d793dbb1887904d896157a9731a8805c17c
Salsabil <salsabeelahmed323@gmail.com>
2024-09-18 02:01:38 +0300

    testt

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,9 @@
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.setEncoding('utf8');
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
+});
 
-process.stdin.on('data', (input) => {
-    const name = input.trim(); // Remove any extra newline characters or spaces
-    console.log(`Your name is: ${name}`);
-    console.log('This important software is now closing');
-    process.exit(); // Exit the program after displaying the message
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
